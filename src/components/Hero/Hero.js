24cb16d755edf745cb41a3480f42e5c3eeb1f1e1
Andrewmat <andre.m.santos1@gmail.com>
2023-07-28 12:34:15 -0300

    Finished unrevised workshop

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
-      <Swoop src="/swoop.svg" />
+      <picture>
+        <source
+          srcSet="
+          /images/hero-img.avif 1x,
+          /images/hero-img@2x.avif 2x,
+          /images/hero-img@3x.avif 3x
+        "
+          type="image/avif"
+        />
+        <source
+          srcSet="
+          /images/hero-img.jpg 1x,
+          /images/hero-img@2x.jpg 2x,
+          /images/hero-img@3x.jpg 3x
+        "
+          type="image/jpg"
+        />
+        <HeroImage
+          src="/images/hero-img.jpg"
+          alt="Hero image of a profile photo of cat looking back"
+        />
+      </picture>
+      <Swoop src="/swoop.svg" alt="" />
     </Wrapper>
   );
 };
--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React, { Fragment } from "react";
+import styled from "styled-components/macro";
 
 const PhotoGridItem = ({ id, src, alt, tags }) => {
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+        <picture>
+          <source
+            srcSet={`
+              ${src.replace(".jpg", ".avif")} 1x,
+              ${src.replace(".jpg", "@2x.avif")} 2x,
+              ${src.replace(".jpg", "@3x.avif")} 3x
+            `}
+            type="image/avif"
+          />
+          <source
+            srcSet={`
+            ${src} 1x,
+            ${src.replace(".jpg", "@2x.jpg")} 2x,
+            ${src.replace(".jpg", "@3x.jpg")} 3x
+          `}
+            type="image/jpg"
+          />
+          <Image src={src} alt={alt} />
+        </picture>
       </Anchor>
       <Tags>
         {tags.map((tag) => (
-          <Tag key={tag}>{tag}</Tag>
+          <Fragment key={tag}>
+            <Tag>{tag}</Tag>{" "}
+          </Fragment>
         ))}
       </Tags>
     </article>
@@ -28,20 +48,31 @@ const Image = styled.img`
   height: 300px;
   border-radius: 2px;
   margin-bottom: 8px;
+
+  object-fit: cover;
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+  display: block;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `;
 
 const Tag = styled.li`
+  display: inline;
   padding: 4px 8px;
+  line-height: calc(1em + 8px);
+  margin-left: 4px;
+  margin-right: 4px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
+
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `;
 
 export default PhotoGridItem;
